16148c2e44856aae0363e995ab7a8bf7a37b4236
praveenbiet <praveenkumar@biet.ac.in>
2025-03-19 11:46:53 +0530

    access Specifiers

---
+++ src/diff/Diffdemo.java
@@ -0,0 +1,12 @@
+package src.diff;
+
+import src.prime.*;
+
+public class Diffdemo{
+    public static void main(String [] args){
+        Prime prime = new Prime();
+        boolean result = prime.isprime(10);
+        System.out.println(result);
+    }
+
+}
---
+++ src/prime/Isprime.java
@@ -0,0 +1,6 @@
+package src.prime;
+
+public class Isprime extends Prime{
+
+
+}
